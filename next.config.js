567a9746fce39f0cec14a4311bce9be77a2319ac
ikramhussainsw <ikram.hussain@magicedtech.com>
2021-04-07 14:07:17 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,50 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
+const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
+const withCSS = require("@zeit/next-css");
+const withSass = require("@zeit/next-sass");
+const nextConfig = {
+  future: {
+    webpack5: true,
+  }
+};
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
-})
+module.exports = withPlugins(
+  [[ withPWA({
+    pwa: {
+      dest: 'public',
+      runtimeCaching,
+    },
+  })],
+    [
+      withCSS({
+        cssModules: true,
+        generateEtags: false,
+        webpack: (config) => {
+          /**some special code */
+          return config;
+        },
+      }),
+    ],
+    [
+      withSass({
+        cssModules: true,
+        generateEtags: false,
+        webpack: (config) => {
+          /**some special code */
+          return config;
+        },
+      }),
+    ],
+    [
+      withImages({
+        generateEtags: false,
+        webpack(config, options) {
+          return config;
+        },
+      }),
+    ],
+  ],
+  nextConfig
+);
